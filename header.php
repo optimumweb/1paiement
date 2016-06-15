3f9ec2ad17771a559a2838ee003e3fad4ddb0a4a
Jonathan Roy <jroy@optimumweb.ca>
2016-06-15 18:44:54 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php get_template_part('head'); ?>
+    <?php wpbp_wrap_before(); ?>
+    <div id="wrap" role="document">
+        <?php wpbp_header_before(); ?>
+        <header id="header" role="banner">
+            <?php wpbp_header_inside_before(); ?>
+            <div class="container">
+                <div class="grid_4">
+                    <h1 id="site-title"><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
+                </div>
+                <div class="grid_8">
+                    <div id="header-widgets">
+                        <?php dynamic_sidebar("Header Widgets"); ?>
+                    </div>
+                </div>
+            </div>
+            <?php wpbp_header_inside_after(); ?>
+        </header>
+        <?php wpbp_header_after(); ?>
+        <nav id="main-nav" role="navigation">
+            <div class="container">
+                <div class="grid_12">
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary_navigation' ) ); ?>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </nav>
