f70bc7f282df60ca0c3a28b04ebffdd1f1e8e537
Jonathan Roy <jroy@optimumweb.ca>
2016-06-17 17:42:26 -0400

    style

--- inc/wpbp-custom.php
+++ inc/wpbp-custom.php
@@ -9,7 +9,7 @@ function enqueue() {
 		wp_enqueue_script('fontawesome', "https://use.fontawesome.com/d85a291cec.js");
 		wp_enqueue_script('theme', THEME_URI . '/js/scripts.js', array('jquery'));
 		// styles
-		wp_enqueue_style('google-fonts', "https://fonts.googleapis.com/css?family=Merriweather+Sans:300,300i,400,400i,700,700i|Roboto:300,300i,400,400i,700,700i");
+		wp_enqueue_style('google-fonts', "https://fonts.googleapis.com/css?family=Merriweather+Sans:300,300i,400,400i,700,700i|Roboto:300,300i,400,400i,700,700i|Cormorant:400,400i");
 		wp_enqueue_style('theme', THEME_URI . '/css/master.css', array('wpbp'));
 	}
 }
