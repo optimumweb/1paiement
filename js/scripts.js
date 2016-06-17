05106d75c2ddc1cb5810cb09ccc0fa32f6cde5f1
Jonathan Roy <jroy@optimumweb.ca>
2016-06-17 11:15:44 -0400

    style

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').each(function() {
+        var $carousel = $(this);
+        var options = {};
+        if ( $carousel.data('owl-carousel-items') !== undefined ) {
+            options.items = $carousel.data('owl-carousel-items');
+        }
+        $carousel.owlCarousel(options);
+    });
 
 });
