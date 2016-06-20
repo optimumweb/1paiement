7fa757857d155b3f5e3e4165e9cff0e23efb4b1d
Jonathan Roy <jroy@optimumweb.ca>
2016-06-20 11:44:39 -0400

    style

---
+++ loop-page.php
@@ -0,0 +1,24 @@
+<?php /* Start loop */ ?>
+<?php while ( have_posts() ) : the_post(); ?>
+    <?php wpbp_post_before(); ?>
+    <article id="page-<?php the_ID(); ?>" class="page">
+        <?php wpbp_post_inside_before(); ?>
+        <?php if ( !get_post_meta(get_the_ID(), 'hide_the_title', true) ) : ?>
+            <header class="page-header" style="<?php echo has_featured_image() ? "background-image: url(" . get_featured_image_url() . ");" : "" ?>">
+                <h1 class="page-title"><?php the_title(); ?></h1>
+                <?php if ( $page_lead = get_post_meta(get_the_ID(), 'page_lead', true) ) : ?>
+                    <p class="lead page-lead"><?php echo $page_lead; ?></p>
+                <?php endif; ?>
+            </header>
+        <?php endif; ?>
+        <section class="page-content">
+            <?php the_content(); ?>
+        </section>
+        <footer class="page-footer">
+            <?php wp_link_pages(array( 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'wpbp'), 'after' => '</p></nav>' )); ?>
+        </footer>
+        <div class="clear"></div>
+        <?php wpbp_post_inside_after(); ?>
+    </article>
+    <?php wpbp_post_after(); ?>
+<?php endwhile; // End the loop ?>
