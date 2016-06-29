a3e110e64761e8b40bad45cda39e136711f33e1e
Jonathan Roy <jroy@optimumweb.ca>
2016-06-28 23:05:55 -0400

    style + dev

--- recent-question.php
+++ recent-question.php
@@ -4,8 +4,8 @@
     <h4 class="question-title">
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </h4>
-    <div class="question-excerpt">
-        <?php the_excerpt(); ?>
+    <div class="question-content">
+        <?php the_content(); ?>
     </div>
     <?php wpbp_post_inside_after(); ?>
 </article>
