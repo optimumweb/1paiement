e89e84f5908b8d5e13734579cc89a5b883d0817d
Jonathan Roy <jroy@optimumweb.ca>
2016-06-28 23:07:07 -0400

    style + dev

--- recent-post.php
+++ recent-post.php
@@ -1,10 +1,10 @@
 <?php wpbp_post_before(); ?>
-<article id="question-<?php the_ID(); ?>" class="question recent-question">
+<article id="post-<?php the_ID(); ?>" class="post recent-post">
     <?php wpbp_post_inside_before(); ?>
-    <h4 class="question-title">
+    <h4 class="post-title">
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </h4>
-    <div class="question-content">
+    <div class="post-content">
         <?php the_content(); ?>
     </div>
     <?php wpbp_post_inside_after(); ?>
